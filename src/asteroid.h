9a6a99290314f45de276d2627f308a27916f4d2e
markus-senger <77236323+markus-senger@users.noreply.github.com>
2021-03-28 19:17:40 +0200

    Update asteroid.h

--- src/asteroid.h
+++ src/asteroid.h
@@ -1,6 +1,6 @@
-// asteroid.h:												MSe, 2021-02-28 
+// asteroid.h:                                              MSe, 2021-02-28 
 // v1.0
-// FH Oberösterreich / Campus Hagenberg
+// FH OberÃ¶sterreich / Campus Hagenberg
 // ----                                                                                                                    
 // ========================================================================
 
